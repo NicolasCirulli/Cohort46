42f0da9dc760e3b0f9c674bb5d492813446f5cff
NicolasCirulli <nicolascirulli2@gmail.com>
2023-04-06 16:01:22 -0300

    playground eventos

--- playground/assets/js/agentes.js
+++ playground/assets/js/agentes.js
@@ -1,27 +1,80 @@
+// referencias
 const $contenedor = document.getElementById( 'contenedor-personajes' )
+const $select = document.getElementById( 'select-js' )
+const $inputBusqueda = document.getElementById( 'busqueda-js' )
 
-
+// datos
 const agentes = personajes.data.filter( agente => agente.isPlayableCharacter )
 
-const imprimirArticulos = ( arrayPersonajes, elementoHTML ) => {
-    elementoHTML.innerHTML = arrayPersonajes.reduce( (acc, act) =>  acc + crearArticle( act ), '' )
-}
+const roles = agentes.map( agente => agente.role.displayName )
 
+const setRoles = new Set( roles )
+
+const arrayRoles = Array.from( setRoles )
+
+// eventos
+
+$select.addEventListener( 'change', () => {
+    const agentesFiltrados = filtroCruzado( agentes, $select.value, $inputBusqueda.value )
+    imprimirArticulos( agentesFiltrados, $contenedor )
+} )
+$inputBusqueda.addEventListener( 'input', () => {
+    const agentesFiltrados = filtroCruzado( agentes, $select.value, $inputBusqueda.value )
+    imprimirArticulos( agentesFiltrados, $contenedor )
+} )
+
+// ejecuciones
 imprimirArticulos( agentes, $contenedor )
+imprimirOptions( arrayRoles, $select  )
 
-
+// funciones
+function imprimirArticulos ( arrayPersonajes, elementoHTML ) {
+    if( arrayPersonajes.length === 0 ){
+        elementoHTML.innerHTML = `<h2> No hay agentes </h2>`
+    }else{
+        elementoHTML.innerHTML = arrayPersonajes.reduce( (acc, act) =>  acc + crearArticle( act ), '' )
+    }
+}
 function crearArticle( agente ){
     return `<article class="card border-dark col-11 col-md-6 col-xl-3">
                 <img class="card-img-top" src="${agente.bustPortrait}" alt="Title">
                 <div class="card-body">
                 <h4 class="card-title">${agente.displayName}</h4>
+                <p class="card-text">${agente.role.displayName}</p>
                 <p class="card-text">${agente.description}</p>
                 </div>
             </article>
     `
 }
+function imprimirOptions( roles, select ){
+    let template = ""
+    for( let role of roles ){
+        template += `<option value="${role}">${role}</option>`
+    }
+    select.innerHTML += template
+} 
+function filtrarPorRol( agentes, rol ){
+    if( rol == "all" ){
+        return agentes
+    }
+    return agentes.filter( agente => agente.role.displayName == rol )
+}
+function filtrarPorTexto( agentes, texto ){
+    return agentes.filter( agente => agente.displayName.toLowerCase().includes( texto.toLowerCase() ) )
+}
+function filtroCruzado( agentes, rol, texto ){
+    const filtradosPorRol = filtrarPorRol( agentes, rol )
+    const filtradosPorTexto = filtrarPorTexto( filtradosPorRol, texto )
+    return filtradosPorTexto
+}
+// datos para crear los options
+/* Hacer dinamicos los options del select */
+// template del option
+/* <option value="Initiator">Initiator</option> */
 
-function imprimirNodos( arrayPersonajes, elementoHTML ){
+
+// ejemplo con nodos
+/* function imprimirNodos( arrayPersonajes, elementoHTML ){
     const $fragment = document.createDocumentFragment()
     for (const personaje of arrayPersonajes) {
         $fragment.appendChild(  crearNodoArticle( personaje )  )
@@ -62,4 +115,4 @@ function crearNodoArticle(agente){
     $article.append( $imagen, $div )
     
     return $article
-}
+} */
